e9a462a376f5290e739c3eb08e130a6c33960ae8
zxx <1834203219@qq.com>
2025-09-04 00:33:42 +0800

    60 - 网关-过滤器-全局过滤器GlobalFilter

---
+++ gateway/src/main/java/com/zxx/filter/RtGlobalFilter.java
@@ -0,0 +1,41 @@
+package com.zxx.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+// 全局过滤器
+@Component
+@Slf4j
+public class RtGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        String uri = request.getURI().toString();
+        long start = System.currentTimeMillis();
+        log.info("请求【{}】开始时间：{}", uri, start);
+        // ======================以上是前置逻辑=====================
+
+        Mono<Void> filter = chain.filter(exchange)
+                .doFinally((result)->{
+                    // =====================以下是后置处理逻辑=================
+                    long end = System.currentTimeMillis();
+                    log.info("请求【{}】结束时间：{}，耗时：{}ms", uri, end, end-start);
+                });
+
+        return filter;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
